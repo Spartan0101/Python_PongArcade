138b1efee881dddcc943662b0004c220e59e7e7c
Spartan1010 <tranulovianos@gmail.com>
2024-08-12 16:52:41 +0100

    Pong - The Famous Arcade Game v2

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG Arcade")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.3)
+    screen.update()
+    ball.move()
+
+screen.exitonclick()
